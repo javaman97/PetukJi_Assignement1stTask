1f3a56a0a68a0e1385fd20618c97e7bcc43ce36a
Aman Nishad <amannishad0512@gmail.com>
2023-03-19 23:53:33 +0530

    Initial commit

---
+++ app/src/main/java/com/example/petukji_assignement1sttask/food.kt
@@ -0,0 +1,7 @@
+package com.example.petukji_assignement1sttask
+
+data class food(var Small:Int,
+                var Medium:Int,
+                var Large:Int,
+                var Category:String
+)
